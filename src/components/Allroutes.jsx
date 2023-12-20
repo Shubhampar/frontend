6eba6d03ce958ff6bcead567900254119a43b63b
Shubhampar <119346050+Shubhampar@users.noreply.github.com>
2023-12-20 16:38:17 +0530

    push

--- src/components/Allroutes.jsx
+++ src/components/Allroutes.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import AddSurveyComponents from './AddSurveyComponents'
+import SurveyListComponent from './SurveyListComponent'
 
 const Allroutes = () => {
   return (
-    <div>Allroutes</div>
+    <div>
+        <Routes>
+        <Route path='/' element={<SurveyListComponent />} />
+        <Route path='/submit' element={<AddSurveyComponents/>} />
+        </Routes>
+    </div>
   )
 }
 
